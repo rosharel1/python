6703930d71c6e657f4c42a25b088d068df63befc
rosharel1 <126953261+rosharel1@users.noreply.github.com>
2023-03-16 07:49:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a):
+  b=int(input())
+  print(a,"+",b,"=",a+b)
+"""def sub(a):
+  print(a,"-",b,"=",a-b)
+def mul(a):
+  print(a,"*",b,"=",a*b)
+def div(a):
+  print(a,"/",b,"=",a/b)
+
+add(int(input()))
